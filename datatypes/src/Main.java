7d181899f4b8fe6b4930b3ed77dd6d23d04227f4
Aysel <109758851+ayslekn@users.noreply.github.com>
2022-09-30 16:22:51 +0300

    Update Project

---
+++ datatypes/src/Main.java
@@ -0,0 +1,14 @@
+package datatypes.src;
+
+public class Main {
+    public static void main(String[] args)
+    {
+       int sayi=125;
+       double sayi1=12.5;
+       sayi=-129;
+       char  karakter='A';
+       boolean dogruMu=false;
+
+
+    }
+}
